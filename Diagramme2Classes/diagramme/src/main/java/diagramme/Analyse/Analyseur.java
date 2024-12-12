13e9849a9892ed1ba77290af8202ae7413fac414
Segard Mathis <mathis.segard6@etu.univ-lorraine.fr>
2024-12-12 15:00:15 +0100

    projet diagramme automatique

--- Diagramme2Classes/diagramme/src/main/java/diagramme/Analyse/Analyseur.java
+++ Diagramme2Classes/diagramme/src/main/java/diagramme/Analyse/Analyseur.java
@@ -1,8 +1,21 @@
 package main.java.diagramme.Analyse;
 
-import java.lang.reflect.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 
 public class Analyseur {
+    private Class analyseClasse;
+
+    public Analyseur(String nomClasse) throws ClassNotFoundException {
+        this.analyseClasse = Class.forName(nomClasse);
+    }
+
+    public void analyseNouvelleClasse(String nomClasse) throws ClassNotFoundException {
+        this.analyseClasse = Class.forName(nomClasse);
+    }
+
     public static void analyseClasse(String nomClasse) {
         try {
             Class cl = Class.forName(nomClasse);
@@ -15,7 +28,7 @@ public class Analyseur {
             e.printStackTrace();
         }
     }
-    
+
 
     public static void afficherAttributs(Class cl) {
         Field[] fields = cl.getDeclaredFields();
@@ -36,4 +49,73 @@ public class Analyseur {
     public static void main(String[] args) {
         analyseClasse("main.java.diagramme.Analyse.Analyseur");
     }
+
+    private static String getModifierVisibilite(int i) {
+        switch (i) {
+            case Modifier.PUBLIC:
+                return "public";
+            case Modifier.PRIVATE:
+                return "private";
+            case Modifier.PROTECTED:
+                return "protected";
+            default:
+                return "Erreur";
+        }
+    }
+
+    private static ArrayList<String> getModifierClasse(int i) {
+        ArrayList<String> modifiers = new ArrayList<>();
+        modifiers.add(getModifierVisibilite(i));
+        if (Modifier.isAbstract(i)) {
+            modifiers.add("abstract");
+        } else if (Modifier.isFinal(i)) {
+            modifiers.add("final");
+        }
+        if (Modifier.isStrict(i)) {
+            modifiers.add("strictfp");
+        }
+        return modifiers;
+    }
+
+    private static ArrayList<String> getModifierMethode(int i) {
+        ArrayList<String> modifiers = new ArrayList<>();
+        modifiers.add(getModifierVisibilite(i));
+        if (Modifier.isStatic(i)) {
+            modifiers.add("static");
+        }
+        if (Modifier.isAbstract(i)) {
+            modifiers.add("abstract");
+        } else {
+            if (Modifier.isNative(i)) {
+                modifiers.add("native");
+            } else if (Modifier.isStrict(i)) {
+                modifiers.add("strictfp");
+            }
+            if (Modifier.isFinal(i)) {
+                modifiers.add("final");
+            }
+            if (Modifier.isSynchronized(i)) {
+                modifiers.add("synchronized");
+            }
+        }
+        return modifiers;
+    }
+
+    private static ArrayList<String> getModifierAttribut(int i) {
+        ArrayList<String> modifiers = new ArrayList<>();
+        modifiers.add(getModifierVisibilite(i));
+        if (Modifier.isStatic(i)) {
+            modifiers.add("static");
+        }
+        if (Modifier.isFinal(i)) {
+            modifiers.add("final");
+        }
+        if (Modifier.isTransient(i)) {
+            modifiers.add("transient");
+        }
+        if (Modifier.isVolatile(i)) {
+            modifiers.add("volatile");
+        }
+        return modifiers;
+}
 }
