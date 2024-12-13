c7469a47bbfa1fdf8854f746ed3e622fbc79d045
Segard Mathis <mathis.segard6@etu.univ-lorraine.fr>
2024-12-13 08:36:24 +0100

    ajout module javafx

--- Diagramme2Classes/diagramme/src/main/java/diagramme/autoload/Autoloader.java
+++ Diagramme2Classes/diagramme/src/main/java/diagramme/autoload/Autoloader.java
@@ -2,6 +2,7 @@ package main.java.diagramme.autoload;
 
 public class Autoloader extends ClassLoader {
     public Class loadClass(String name) throws ClassNotFoundException {
+
         return findClass(name);
     }
 }
