44d1a915d53fda841a145097ba7c9ca2aef30319
Jules.Adr <jules.adr@orange.fr>
2024-12-13 08:48:19 +0100

    Classe de test

--- Diagramme2Classes/Test/main/java/diagramme/Analyse/AnalyseurTest.java
+++ Diagramme2Classes/Test/main/java/diagramme/Analyse/AnalyseurTest.java
@@ -1,8 +1,10 @@
 package main.java.diagramme.Analyse;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.List;
 import java.util.Map;
@@ -11,20 +13,74 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AnalyseurTest {
 
+
+    private Analyseur analyseur;
+
+    @BeforeEach
+    void setUp() throws ClassNotFoundException {
+        analyseur = new Analyseur("main.java.diagramme.Analyse.Analyseur");
+    }
+
+    @Test
+    void testGetNomClasse() {
+        String nomClasse = analyseur.getNomClasse();
+        assertEquals("main.java.diagramme.Analyse.Analyseur", nomClasse);
+    }
+
+    @Test
+    void testGetClasseParent() {
+        Class<?> classeParent = analyseur.getClasseParent();
+        assertNotNull(classeParent);
+        assertEquals(Object.class, classeParent);
+    }
+
+    @Test
+    void testGetInterfaces() {
+        Class<?>[] interfaces = analyseur.getInterfaces();
+        assertNotNull(interfaces);
+        assertEquals(0, interfaces.length);
+    }
+
     @Test
     void trierAttributsParModificateur() throws ClassNotFoundException {
         Analyseur a = new Analyseur("main.java.diagramme.Analyse.Analyseur");
-         Map<String, List<Field>> test =a.trierAttributsParModificateur();
-         for (String key : test.keySet()) {
-             if(test.get(key).size() != 0) {
-                 assertEquals("private", key);
-             }
-             for (Field f : test.get(key)) {
-                    assertEquals("analyseClasse", f.getName());
-                    assertEquals("java.lang.Class", f.getType().getName());
-                    assertEquals(Modifier.PRIVATE, f.getModifiers());
-                    assertEquals(f.getType().getSuperclass().getName(),"java.lang.Object");
-             }
-         }
+        Map<String, List<Field>> test = a.trierAttributsParModificateur();
+        for (String key : test.keySet()) {
+            if (!test.get(key).isEmpty()) {
+                assertEquals("private", key);
+            }
+            for (Field f : test.get(key)) {
+                assertEquals("analyseClasse", f.getName());
+                assertEquals("java.lang.Class", f.getType().getName());
+                assertEquals(Modifier.PRIVATE, f.getModifiers());
+                assertEquals(f.getType().getSuperclass().getName(), "java.lang.Object");
+            }
+        }
+    }
+
+    @Test
+    void testTrierMethodesParModificateur() {
+        Method[] methods = analyseur.getClass().getDeclaredMethods();
+        Map<String, List<Method>> triMethodes = analyseur.trierMethodesParModificateur(methods);
+        assertNotNull(triMethodes);
+        assertTrue(triMethodes.containsKey("public"));
+        assertTrue(triMethodes.containsKey("protected"));
+        assertTrue(triMethodes.containsKey("private"));
+    }
+
+    @Test
+    void AfficherElements() throws ClassNotFoundException {
+        Analyseur a = new Analyseur("main.java.diagramme.Analyse.Analyseur");
+        Class<?> cl = Class.forName("main.java.diagramme.Analyse.Analyseur");
+        Analyseur.afficherAttributs(cl);
+        Analyseur.afficherMethodes(cl);
+
+    }
+
+    @Test
+    void testGetPackages() {
+        Package[] packages = Analyseur.getPackages();
+        assertNotNull(packages);
+        assertTrue(packages.length > 0);
     }
 }
